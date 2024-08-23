a9d07a4b7bf05d6beb14c1586cd089b55575de99
Arturo Quiros <adquirose@gmail.com>
2024-08-23 18:21:30 -0400

    krpano --> public

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 import axios from 'axios'
 
 const obtenerLotes = async() => {
-  const response = await axios.get('https://rodan.com.py/propiedades/fraccion/375/lotes',)
+  const response = await axios.get('https://rodan.com.py/propiedades/fraccion/375/lotes')
   console.log(response)
 }
 obtenerLotes()
