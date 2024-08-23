bec575e62dff030fada3808a6b66a531e511d9ba
Arturo Quiros <adquirose@gmail.com>
2024-08-23 18:07:13 -0400

    inicio

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import axios from 'axios'
+
+const obtenerLotes = async() => {
+  const response = await axios.get('https://rodan.com.py/propiedades/fraccion/375/lotes',)
+  console.log(response)
+}
+obtenerLotes()
+embedpano({ xml:'./krpano/tour.xml', target:'pano', html5:"only" })
+
+
