e58246f45f2c3ffc91fa7a717dd22f42a49db4c5
Arturo Quiros <adquirose@gmail.com>
2024-08-26 13:19:09 -0400

    fix1

--- main.js
+++ main.js
@@ -2,15 +2,9 @@ import './style.css'
 import axios from 'axios'
 
 const obtenerLotes = async() => {
-  const response = await axios.get('https://rodan.com.py/propiedades/fraccion/375/lotes',{
-    headers: { 
-      "Content-Type": "application/json",
-      'Access-Control-Allow-Origin' : '*',
-    },
-  })
+  const response = await axios.get('https://rodan.com.py/propiedades/fraccion/375/lotes')
   console.log(response)
 }
-obtenerLotes()
-embedpano({ xml:'./krpano/tour.xml', target:'pano', html5:"only" })
+embedpano({ xml:'./krpano/tour.xml', target:'pano', html5:"only", onready:obtenerLotes })
 
 
